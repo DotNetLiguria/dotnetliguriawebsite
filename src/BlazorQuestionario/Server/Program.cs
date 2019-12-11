2d0a2fe0b62c1a731169849622d352a3bfab822b
Raf <raffaeler@vevy.com>
2019-12-11 16:58:52 +0100

    Added support for https Added support for protecting secrets on a separate configuration file in a different folder Created a deploy readme with step-by-step instructions

--- src/BlazorQuestionario/Server/Program.cs
+++ src/BlazorQuestionario/Server/Program.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BlazorAppTest.Server
 {
@@ -11,12 +19,129 @@ namespace BlazorAppTest.Server
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var mainAssembly = typeof(Program).Assembly.GetName().Name; // BlazorAppTest.Server
+            mainAssembly = mainAssembly.Replace(".", "").Replace("-", "");
+            var configEnv = $"{mainAssembly}Config";
+            Console.WriteLine($"Looking for {configEnv}");
+            var deployfile = Environment.GetEnvironmentVariable(configEnv);
+            Console.WriteLine($"Found extra config: {deployfile}");
+            // deployfile string is a full path to an additional configuration like:
+            // "/home/username/edp/BlazorAppTest.Server.json"
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    if (deployfile != null)
+                    {
+                        options.ConfigureEndpoints();
+                    }
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    if (deployfile != null)
+                    {
+                        config.AddJsonFile(deployfile, optional: true, reloadOnChange: true);
+                    }
+                })
                 .UseConfiguration(new ConfigurationBuilder()
                     .AddCommandLine(args)
                     .Build())
                 .UseStartup<Startup>()
                 .Build();
+        }
+    }
+
+    public static class KestrelServerOptionsExtensions
+    {
+        public static void ConfigureEndpoints(this KestrelServerOptions options)
+        {
+            var configuration = options.ApplicationServices.GetRequiredService<IConfiguration>();
+            var environment = options.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+
+            var endpoints = configuration.GetSection("HttpServer:Endpoints")
+                .GetChildren()
+                .ToDictionary(section => section.Key, section =>
+                {
+                    var endpoint = new EndpointConfiguration();
+                    section.Bind(endpoint);
+                    return endpoint;
+                });
+
+            foreach (var endpoint in endpoints)
+            {
+                var config = endpoint.Value;
+                var port = config.Port ?? (config.Scheme == "https" ? 443 : 80);
+
+                var ipAddresses = new List<IPAddress>();
+                if (config.Host == "localhost")
+                {
+                    ipAddresses.Add(IPAddress.IPv6Loopback);
+                    ipAddresses.Add(IPAddress.Loopback);
+                }
+                else if (IPAddress.TryParse(config.Host, out var address))
+                {
+                    ipAddresses.Add(address);
+                }
+                else
+                {
+                    ipAddresses.Add(IPAddress.IPv6Any);
+                }
+
+                foreach (var address in ipAddresses)
+                {
+                    options.Listen(address, port,
+                        listenOptions =>
+                        {
+                            if (config.Scheme == "https")
+                            {
+                                var certificate = LoadCertificate(config, environment);
+                                listenOptions.UseHttps(certificate);
+                            }
+                        });
+                }
+            }
+        }
+
+        private static X509Certificate2 LoadCertificate(EndpointConfiguration config, IWebHostEnvironment environment)
+        {
+            if (config.StoreName != null && config.StoreLocation != null)
+            {
+                using (var store = new X509Store(config.StoreName, Enum.Parse<StoreLocation>(config.StoreLocation)))
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    var certificate = store.Certificates.Find(
+                        X509FindType.FindBySubjectName,
+                        config.Host,
+                        validOnly: !environment.IsDevelopment());
+
+                    if (certificate.Count == 0)
+                    {
+                        throw new InvalidOperationException($"Certificate not found for {config.Host}.");
+                    }
+
+                    return certificate[0];
+                }
+            }
+
+            if (config.FilePath != null && config.Password != null)
+            {
+                return new X509Certificate2(config.FilePath, config.Password);
+            }
+
+            throw new InvalidOperationException("No valid certificate configuration found for the current endpoint.");
+        }
+    }
+
+    public class EndpointConfiguration
+    {
+        public string Host { get; set; }
+        public int? Port { get; set; }
+        public string Scheme { get; set; }
+        public string StoreName { get; set; }
+        public string StoreLocation { get; set; }
+        public string FilePath { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ src/BlazorQuestionario/Server/Readme.md
@@ -0,0 +1,80 @@
+﻿## Deploying Notes
+December 11, 2019 - @raffaeler
+
+### Publishing on Linux
+1. Create a publishing folder
+```
+dotnet publish -c Release -r linux-x64 --self-contained true
+```
+
+2. Modify the Blazor configuration named `BlazorAppTest.Client.blazor.config`. The last line must specify a full qualified folder.
+For example:
+```
+/home/username/BlazorAppTest/BlazorAppTest.Client.dll
+```
+
+3. Using SSH copy the published application to Linux
+
+4. Enter the linux folder and give the execution permissions to the executable:
+```
+chmod +x BlazorAppTest.Server
+```
+
+5. On Linux copy the dist folder in the application root:
+```
+Source: /home/username/BlazorAppTest/BlazorAppTest.Client/dist
+Target: /home/username/BlazorAppTest/dist
+```
+
+6. On Windows, create a self-signed certificate:
+
+```
+dotnet dev-certs https -ep .\cert.pfx -p ComplexPassword 
+```
+
+7. Copy the certificate in a location on the Linux machine (than the publishing folder)
+For example: /home/secure/cert.pfx
+
+8. Create a json file in the `/home/secure/` folder with the https configuration:
+```
+{
+  "HttpServer": {
+    "Endpoints": {
+      "Http": {
+        "Host": "0.0.0.0",
+        "Port": 5000,
+        "Scheme": "http"
+      },
+      "Https": {
+        "Host": "0.0.0.0",
+        "Port": 5001,
+        "Scheme": "https",
+        "FilePath": "/home/secure/cert.pfx",
+        "Password": "ComplexPassword"
+      }
+    }
+  }
+}
+```
+
+9. Right before running the server, instruct the app about the existence of the new configuration file:
+```
+export BlazorAppTestServerConfig="/home/secure/BlazorAppTest.Server.json"
+```
+
+Please note that the "BlazorAppTestServerConfig" environment variable is computed dynamically.
+It is the AssemblyName without '.' or '-' with "Config" appended at the end.
+
+10. If the file does **not** exist, the normal ASP.NET Core configuration is used.
+In this case, you can change the listening URLS and ports with
+  * ASPNETCORE_URLS="https://0.0.0.0:5001;http://0.0.0.0:5000"
+
+11. Optional: make the app terminal log verbose or silent with either of the two:
+```
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_ENVIRONMENT=Production
+```
+
+12. Run the app
+
+
