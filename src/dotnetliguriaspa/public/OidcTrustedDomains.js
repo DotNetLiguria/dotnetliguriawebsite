100e8a45b2305ca48b17d5ba8c43db7363545e45
raffaeler <raffaeler@vevy.com>
2023-03-13 18:28:59 +0100

    minor changes

--- src/dotnetliguriaspa/public/OidcTrustedDomains.js
+++ src/dotnetliguriaspa/public/OidcTrustedDomains.js
@@ -13,10 +13,10 @@ const trustedDomains = {
         "https://localhost:5443/",
         new RegExp('^(https://[a-zA-Z0-9-]+.dotnetliguria.net/api/)')
     ],
-    config_classic: ['https://demo.duendesoftware.com'],
-    config_without_silent_login: ['https://demo.duendesoftware.com'],
-    config_without_refresh_token: ['https://demo.duendesoftware.com'],
-    config_without_refresh_token_silent_login: ['https://demo.duendesoftware.com'],
+    // config_classic: ['https://demo.duendesoftware.com'],
+    // config_without_silent_login: ['https://demo.duendesoftware.com'],
+    // config_without_refresh_token: ['https://demo.duendesoftware.com'],
+    // config_without_refresh_token_silent_login: ['https://demo.duendesoftware.com'],
     config_google: ['https://oauth2.googleapis.com', 'https://openidconnect.googleapis.com'],
-    config_with_hash: ['https://demo.duendesoftware.com'],
+    // config_with_hash: ['https://demo.duendesoftware.com'],
 };
